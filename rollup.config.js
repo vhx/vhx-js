d937abaed28979a22b312b5b9804151d02b3e0bf
davegonzalez <david@vhx.tv>
2017-06-02 15:54:16 -0400

    Finished a rewrite of the JS Client SDK - Bundle is generated in UMD format so that this can be used in both node and in a browser - Can be easily imported into ES6 modules with import VhxApi from 'vhxjs' - Now uses rollup instead of gulp for the build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,28 @@
 import buble from 'rollup-plugin-buble';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
+import conditional from 'rollup-plugin-conditional';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   entry: './lib/vhx.js',
   format: 'umd',
-  dest: './dist/bundle.js',
+  dest: './dist/vhx.js',
   moduleName: 'VhxApi',
-  external: ['superagent'],
+  sourceMap: true,
   plugins: [
-		nodeResolve({
+    nodeResolve({
       browser: true,
       jsnext: true
     }),
-		commonjs(),
-		buble()
-	]
+    commonjs(),
+    buble(),
+    conditional(isProduction, [
+      uglify({
+        sourceMap: true
+      })
+    ])
+  ]
 };
